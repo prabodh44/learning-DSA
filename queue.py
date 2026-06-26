18101825d48e892358fc2a93b176909e15583be3
Prabodh Tuladhar <prabodh44@users.noreply.github.com>
2026-06-26 17:58:10 +0545

    added the enqueue functionality

--- queue.py
+++ queue.py
@@ -14,17 +14,39 @@ class Queue:
     
     """ enqueue method
     """
-    def enqueue(item):
-        pass
+    def enqueue(self, item):
+        try:
+            if not self.is_full():
+                if self.head == -1:
+                    self.head = 0
+                    self.tail = 0
+                    self.queue[self.tail] = item
+                else:
+                    self.tail += 1
+                    self.queue[self.tail] = item
+                    
+                print(f"Item added is {item}")
+                print(f"head: {self.head}")
+                print(f"tail: {self.tail}")
+                
+        except IndexError:
+            print("The queue is full")
     
     def is_empty(self):
         if self.head == self.tail == -1:
             raise IndexError("The Queue is empty")
+    def is_full(self):
+        if self.tail == self.size - 1:
+            raise IndexError("The Queue is full")
     
     def show(self):
         print(self.queue)
 
 if __name__ == "__main__":
     q = Queue(5)
-    q.is_empty()
-    q.show()
+    q.enqueue(3)
+    q.enqueue(4)
+    q.enqueue(5)
+    q.enqueue(4)
+    q.enqueue(5)
+    q.enqueue(14)
