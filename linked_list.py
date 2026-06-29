f7438981d4697cf225ca297d391438f79bda686e
prabodh44 <prabodhtuladhar44@gmail.com>
2026-06-29 17:25:41 +0545

    Added the insert at the beginning method as well as added the __repr__ function for easy debugging

--- linked_list.py
+++ linked_list.py
@@ -9,10 +9,37 @@ class Node:
     def __init__(self, item):
         self.item = item
         self.next = None
+    
+    def __str__(self):
+        return f"Item: {self.item} - Next: {self.next}"
+    
+    def __repr__(self):
+        return f"Item: {self.item} - Next: {self.next}"
 
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def __repr__(self):
+        return f"LinkedList class: self.head value: {self.head}"
+    
+    # Linked List functionalities: Traversal, Insert, Delete, Search and Sort
+    def traverse_linked_list(self):
+        # TODO: Understand this code??
+        # temp variable to store the head 
+        current = self.head
+        # display items in the linked list
+        while current is not None:
+            print(f"The item is node in {current.item}")
+            current = current.next
+    
+    # Insert methods
+    def insert_at_front(self, data):
+        # create a new node
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
 
 if __name__ == "__main__":
     # create linked list
@@ -22,7 +49,7 @@ if __name__ == "__main__":
     first_node = Node("First Node")
     second_node = Node("Second Node")
     third_node = Node("Third Node")
-
+    
     # connect the first node to head
     linked_list.head = first_node
     print(f"HEAD value: {linked_list.head}")
@@ -44,7 +71,16 @@ if __name__ == "__main__":
     # no need to write this code since all nodes
     # point to None when they are initialized in the __init__ method
 
-    # display items in the linked list
-    while linked_list.head != None:
-        print(f"The item is node in {linked_list.head.item}")
-        linked_list.head = linked_list.head.next
+    # linked list methods
+    # 1. Traversal
+    linked_list.traverse_linked_list()
+
+    # # 2. Insert linked list at the beginning
+    # linked_list.insert_at_front("Zero Node")
+
+    # linked_list.insert_at_front("Before Zero Node")
+
+    # print("****** AFTER NEW NODES INSERTION *******")
+    
+    # linked_list.traverse_linked_list()
+    
